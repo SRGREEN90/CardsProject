f5b8925f2317ef2a93b9424f079b4188a7e4a217
nastassia <anastasiyamihalenko@gmail.com>
2022-02-27 23:41:35 +0300

    added styles for common components

--- src/features/Test.tsx
+++ src/features/Test.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import SuperButton from "../main/ui/common/SuperButton/SuperButton";
 import SuperCheckbox from "../main/ui/common/SuperCheckbox/SuperCheckbox";
 import SuperInputText from "../main/ui/common/SuperInputText/SuperInputText";
+import s from './test.module.css';
 
 export const Test = () => {
     return (
-        <div>
+        <div className={s.container}>
             <SuperButton>BUTTON</SuperButton>
             <SuperCheckbox/>
             <SuperInputText/>
