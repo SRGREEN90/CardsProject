b19e29688085b21f1f508e1835fdf9c79b79628b
Anatol-T <84692030+Anatol-T@users.noreply.github.com>
2022-03-05 23:28:02 +0800

    button

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import SuperButton from "../../main/ui/common/SuperButton/SuperButton";
 
 export const Profile = () => {
     return (
         <div>
             Profile
+          <SuperButton>Button</SuperButton>
         </div>
     );
 };
