016641374bb5f3ed5d592db90c5b39410368c028
nastassia <anastasiyamihalenko@gmail.com>
2022-03-05 18:12:52 +0300

    added button for login

--- src/features/auth/Login/Login.tsx
+++ src/features/auth/Login/Login.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import SuperButton from "../../../main/ui/common/SuperButton/SuperButton";
 
 export const Login = () => {
     return (
         <div>
             Login
+            <SuperButton>Login</SuperButton>
         </div>
     );
 };
