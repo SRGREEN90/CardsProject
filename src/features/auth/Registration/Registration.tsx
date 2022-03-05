416b4a531dc7e589f23c05d108be6b3174e734d8
katrina <katrina.nov13@gmail.com>
2022-03-05 18:07:49 +0300

    added inputs

--- src/features/auth/Registration/Registration.tsx
+++ src/features/auth/Registration/Registration.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import SuperButton from "../../../main/ui/common/SuperButton/SuperButton";
+import SuperInputText from "../../../main/ui/common/SuperInputText/SuperInputText";
 
 export const Registration = () => {
     return (
         <div>
             Registration
+            <SuperInputText/>
+            <SuperInputText/>
             <SuperButton>Register</SuperButton>
         </div>
     );
