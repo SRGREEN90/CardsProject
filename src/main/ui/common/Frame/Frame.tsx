f1eba6598a09a5d37db8adaea54423498a482389
nastassia <anastasiyamihalenko@gmail.com>
2022-03-08 00:40:33 +0300

    added styles for Frame

--- src/main/ui/common/Frame/Frame.tsx
+++ src/main/ui/common/Frame/Frame.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import stl from './Frame.module.css'
+import styles from './Frame.module.css';
 
 type PropsType = {
-  children:  React.ReactNode
+    children: React.ReactNode
 }
-export const Frame = (props:PropsType) => {
-  return (
-    <div className={stl.wrapper}>
-      <div className={stl.frame}>
-        {props.children}
-      </div>
-    </div>
-  );
+export const Frame = (props: PropsType) => {
+    return (
+        <div className={styles.wrapper}>
+            <div className={styles.frame}>
+                {props.children}
+            </div>
+        </div>
+    );
 };
