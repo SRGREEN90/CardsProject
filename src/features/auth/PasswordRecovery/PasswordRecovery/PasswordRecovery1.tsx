f8b66e7474477a5e4645adfe7b5fb85724674c72
nastassia <anastasiyamihalenko@gmail.com>
2022-03-10 17:20:59 +0300

    page error output + styles

--- src/features/auth/PasswordRecovery/PasswordRecovery/PasswordRecovery1.tsx
+++ src/features/auth/PasswordRecovery/PasswordRecovery/PasswordRecovery1.tsx
@@ -4,14 +4,16 @@ import SuperInputPassword from "../../../../main/ui/common/SuperInputPassword/Su
 import SuperButton from "../../../../main/ui/common/SuperButton/SuperButton";
 import {useDispatch, useSelector} from "react-redux";
 import {changePassTC} from "../../../../main/bll/newPasswordReducer";
-import {Navigate, useParams} from "react-router-dom";
+import {Navigate, NavLink, useParams} from "react-router-dom";
 import {AppRootStateType} from "../../../../main/bll/store";
 import {PATH} from "../../../../main/ui/routes/Routes";
+import styles from "./passwordRecovery1.module.css";
 
 
 export const PasswordRecovery1 = () => {
     const [password, setPassword] = useState<string>('');
     const isChangedPass = useSelector<AppRootStateType, boolean>(state => state.newPassword.isChangedPass);
+    const error = useSelector<AppRootStateType, string | null>(state => state.newPassword.error);
     const dispatch = useDispatch()
     const {token} = useParams<{ token: string }>();
 
@@ -28,7 +30,12 @@ export const PasswordRecovery1 = () => {
             <Frame>
                 <span><strong>It-incubator</strong></span>
                 <h2>Create new password</h2>
-                <div>
+                {error && <div className={styles.error}>
+                    <p>{error}</p>
+                    <NavLink to={PATH.FORGOT_YOUR_PASSWORD} className={styles.linkBack}>
+                        <p className={styles.forgotText}>Back to reset pass</p></NavLink>
+                </div>}
+                <div className={styles.input}>
                     <label>
                         Password
                     </label>
