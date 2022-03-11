0df9de0bcae8bc85b1881cb010666316b3541cb4
nastassia <anastasiyamihalenko@gmail.com>
2022-03-11 16:58:14 +0300

    pagePage

--- src/API/api.ts
+++ src/API/api.ts
@@ -1,7 +1,7 @@
 import axios, {AxiosResponse} from 'axios';
 
 export const instance = axios.create({
-    baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/',
+    baseURL: 'https://neko-back.herokuapp.com/2.0' || 'http://localhost:7542/2.0/',
     withCredentials: true,
 })
 
@@ -27,7 +27,7 @@ export const cardsAPI = {
                 <img src="https://img.icons8.com/external-bearicons-detailed-outline-bearicons/64/000000/external-Letter-business-and-marketing-bearicons-detailed-outline-bearicons.png" alt="#"/>
             </div>
             <p style="font-size: 17px; color: #2D2E46">We heard you need a password reset. Click the link below, and you'll be redirected to a site from which you can set a new password.</p>
-            <a style="text-decoration: none; color: white; background-color: #21268F; outline: none; border: none; padding: 15px 20px;border-radius: 7px"href='http://localhost:3000/#/set-new-password/$token$'>Reset password</a>
+            <a style="text-decoration: none; color: white; background-color: #21268F; outline: none; border: none; padding: 15px 20px;border-radius: 7px"https://nastassiamikhalenka.github.io/projectcards/#/set-new-password/$token$'>Reset password</a>
         </div>`
         })
     },
