81cd37abd753324d75cfdae1baec67fe41732b59
nastassia <anastasiyamihalenko@gmail.com>
2022-03-14 00:06:42 +0300

    added header for packs/profile (with styles)

--- src/main/ui/header/Header.tsx
+++ src/main/ui/header/Header.tsx
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavLink} from "react-router-dom";
 import {PATH} from "../routes/Routes";
 import styles from './Header.module.css';
+import profileIcon from '../../../assets/images/Profile.png'
+import packsListIcon from '../../../assets/images/Packslist.png'
 
 const Header = () => {
+    const [isActive, setIsActive] = useState<'profile' | 'packs'>('packs')
+
+    const classActiveProfile = `${isActive === "profile" ? styles.isActive : ''}`
+    const classActivePacks = `${isActive === "packs" ? styles.isActive : ''}`
+
+
     return (
         <header className={styles.header}>
             <nav>
-                <NavLink to={PATH.TEST}>TEST </NavLink>
-                <NavLink to={PATH.PROFILE}>PROFILE </NavLink>
-                <NavLink to={PATH.LOGIN}>LOGIN </NavLink>
-                <NavLink to={PATH.REGISTRATION}>REGISTRATION </NavLink>
-                <NavLink to={PATH.REGISTRATION}>REGISTRATION </NavLink>
+                {/*<NavLink to={PATH.TEST}>TEST </NavLink>*/}
+                <NavLink to={PATH.PACKS} className={classActivePacks} onClick={() => setIsActive('packs')}><img src={packsListIcon} alt={'packsListIcon'}/>Packs list</NavLink>
+                <NavLink to={PATH.PROFILE} className={classActiveProfile} onClick={() => setIsActive('profile')}><img src={profileIcon} alt={'profileIcon'}/>Profile </NavLink>
+                {/*<NavLink to={PATH.LOGIN}>LOGIN </NavLink>*/}
+                {/*<NavLink to={PATH.REGISTRATION}>REGISTRATION </NavLink>*/}
+                {/*<NavLink to={PATH.REGISTRATION}>REGISTRATION </NavLink>*/}
 
-                <NavLink to={PATH.FORGOT_YOUR_PASSWORD}>FORGOT_YOUR_PASSWORD </NavLink>
+                {/*<NavLink to={PATH.FORGOT_YOUR_PASSWORD}>FORGOT_YOUR_PASSWORD </NavLink>*/}
 
-                <NavLink to={PATH.PASSWORD_RECOVERY}>PASSWORD_RECOVERY </NavLink>
+                {/*<NavLink to={PATH.PASSWORD_RECOVERY}>PASSWORD_RECOVERY </NavLink>*/}
 
-                <NavLink to={PATH.CHECK_EMAIL}>CHECK_EMAIL</NavLink>
+                {/*<NavLink to={PATH.CHECK_EMAIL}>CHECK_EMAIL</NavLink>*/}
             </nav>
         </header>
     )
