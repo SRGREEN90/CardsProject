e891d09d63f14d1ed6d76fa5a6b0496f6b779dfc
nastassia <anastasiyamihalenko@gmail.com>
2022-03-13 23:26:15 +0300

    added superRange in sidebar (with styles)

--- src/main/ui/Sidebar/Sidebar.tsx
+++ src/main/ui/Sidebar/Sidebar.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "./Sidebar.module.css";
 import {DoubleCheckbox} from "../common/GridinCheckbox/DoubleCheckbox";
+import {RangeDemo} from "../common/SuperRange/RangeDemo";
 
 const Sidebar = () => {
+
     return (
         <div className={styles.sidebar}>
             Show packs cards
             <DoubleCheckbox/>
-            Двойной ползунок
+            <RangeDemo/>
         </div>
     );
 };
