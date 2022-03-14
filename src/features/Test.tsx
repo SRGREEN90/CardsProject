62f6edaae6dc9dd542ee797ddf0dfadcdff071a7
Anatol-T <84692030+Anatol-T@users.noreply.github.com>
2022-03-14 17:22:28 +0800

    component pagination done

--- src/features/Test.tsx
+++ src/features/Test.tsx
@@ -7,34 +7,42 @@ import SuperEditableSpan from "../main/ui/common/SuperEditableSpan/SuperEditable
 import {RangeDemo} from "../main/ui/common/SuperRange/RangeDemo";
 import {DoubleCheckbox} from "../main/ui/common/GridinCheckbox/DoubleCheckbox";
 import {Search} from "../main/ui/common/GridinSearch/Search";
+import {Pagination} from "../main/ui/common/Pagination/Pagination";
 
 export const Test = () => {
-    const [value, setValue] = useState<string>('')
+  const [value, setValue] = useState<string>('')
+  const [page, setPage] = useState(1)
+  const onChangedPage = (page: number) => {
+    setPage(page)
+  }
+  return (
+    <div className={s.container}>
+      <div>
+        <Pagination totalCount={2211} pageSize={10} currentPage={page} onChangedPage={onChangedPage}/>
+      </div>
+      <SuperButton>BUTTON</SuperButton>
+      <SuperCheckbox/>
+      <SuperInputText/>
+      <div>
+        <SuperEditableSpan
+          value={value}
+          onChangeText={setValue}
+          spanProps={{children: value ? undefined : 'enter text...'}}
+        />
 
-    return (
-        <div className={s.container}>
-            <SuperButton>BUTTON</SuperButton>
-            <SuperCheckbox/>
-            <SuperInputText/>
-            <div>
-                <SuperEditableSpan
-                    value={value}
-                    onChangeText={setValue}
-                    spanProps={{children: value ? undefined : 'enter text...'}}
-                />
-
-                <h3>Double Check</h3>
-                <div>
-                    <DoubleCheckbox/>
-                </div>
-
-                <h3>Search</h3>
-                <div>
-                    <Search/>
-                </div>
-
-            </div>
-          <RangeDemo/>
+        <h3>Double Check</h3>
+        <div>
+          <DoubleCheckbox/>
         </div>
-    );
+
+        <h3>Search</h3>
+        <div>
+          <Search/>
+        </div>
+
+      </div>
+      <RangeDemo/>
+
+    </div>
+  );
 };
---
+++ src/main/ui/common/Pagination/Pagination.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import stl from './Pagination.module.css';
+
+type PropsType = {
+  totalCount: number
+  pageSize: number
+  currentPage: number
+  onChangedPage: (n: number) => void
+}
+export const Pagination = ({totalCount, pageSize, currentPage, onChangedPage}: PropsType) => {
+  //const isLoading = useSelector<AppRootStateType, boolean>(state => state.app.isLoading);
+  const pageCounts = totalCount ? Math.ceil(totalCount / pageSize) : 1;
+  const pages = [];
+  const step = pageCounts > 200 ? 50 : 10;
+  const isStep = pageCounts > 10;
+
+  let pageLimit = 6;
+  let startPage = currentPage - pageLimit / 2;
+  let endPage = currentPage + pageLimit / 2;
+
+  if (startPage < 1) {
+    startPage = 1;
+    endPage = pageLimit + 1;
+  }
+  if (endPage > pageCounts) {
+    endPage = pageCounts;
+    startPage = pageCounts - pageLimit < 1 ? 1 : pageCounts - pageLimit;
+  }
+  for (let i = startPage; i <= endPage; i++) {
+    pages.push(i);
+  }
+
+  const pageList = pages.map(n => {
+    const onClickGetByPage = () => onChangedPage(n);
+    //const onClickGetByPage = () => !isLoading && onChangedPage(n);
+
+    return (
+      <span key={n}
+            className={currentPage === n ? stl.currentPage : stl.page}
+            onClick={onClickGetByPage}>
+                {n}
+            </span>
+    );
+  })
+
+  //Functions Buttons
+  const firstPageHandler = () => onChangedPage(1);
+  const lastPageHandler = () => onChangedPage(pageCounts);
+  const upPageHandler = () => {
+    (currentPage + step) > pageCounts
+      ? onChangedPage(pageCounts)
+      : onChangedPage(currentPage + step)
+  };
+  const downPageHandler = () => {
+    (currentPage - step) < 0
+      ? onChangedPage(1)
+      : onChangedPage(currentPage - step)
+  };
+
+  //COMPLETE JSX
+  return (
+    <div className={stl.pagesWrapper}>
+      <span className={stl.page} onClick={firstPageHandler}>{'<<'}</span>
+      {isStep && <span className={stl.page} onClick={downPageHandler}>{'--'}</span>}
+      <div className={stl.pageList}>
+        {pageList}
+      </div>
+      {isStep && <span className={stl.page} onClick={upPageHandler}>{'++'}</span>}
+      <span className={stl.page} onClick={lastPageHandler}>{'>>'}</span>
+    </div>
+  );
+}
