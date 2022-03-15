8dc6c0ec9bf8b5955dbc3d86575f1764bc57afed
Anatol-T <84692030+Anatol-T@users.noreply.github.com>
2022-03-15 13:00:48 +0800

    sorting done

--- src/features/packsList/PacksTable/Table/PacksTable.tsx
+++ src/features/packsList/PacksTable/Table/PacksTable.tsx
@@ -1,4 +1,4 @@
-import React, {MouseEvent, useState} from 'react';
+import React from 'react';
 import styles from './PacksTable.module.css'
 import Pack from "./Pack";
 import {useDispatch, useSelector} from "react-redux";
@@ -7,46 +7,50 @@ import {PackType} from "../../../../API/cardsPackApi";
 import {sortPacksAC} from "../../../../main/bll/cardsPackReducer";
 
 const PacksTable = () => {
-    const dispatch = useDispatch();
-    const packs = useSelector<AppRootStateType, Array<PackType>>(state => state.cardsPack.cardPacks)
-    const sortPacks = useSelector<AppRootStateType, string>(state => state.cardsPack.sortPacks)
+  const dispatch = useDispatch();
+  const packs = useSelector<AppRootStateType, Array<PackType>>(state => state.cardsPack.cardPacks)
+  const sortPacks = useSelector<AppRootStateType, string>(state => state.cardsPack.sortPacks)
 
-    // const [isActive, setIsActive] = useState<boolean>(false)
+  const direction = sortPacks[0]
+  const activeField = sortPacks.slice(1)
+  const rotate = direction === "1" ? styles.up : ""
 
-    const sortFields = (field: string) => {
+  // const [isActive, setIsActive] = useState<boolean>(false)
 
-        if (sortPacks.slice(1) !== field) {
-            dispatch(sortPacksAC('0' + field))
-        } else {
-            if (sortPacks[0] !== '0') {
-                dispatch(sortPacksAC('0' + field))
-            } else {
-                dispatch(sortPacksAC('1' + field))
-            }
-        }
+  const sortFields = (field: string) => {
+
+    if (sortPacks.slice(1) !== field) {
+      dispatch(sortPacksAC('0' + field))
+    } else {
+      if (sortPacks[0] !== '0') {
+        dispatch(sortPacksAC('0' + field))
+      } else {
+        dispatch(sortPacksAC('1' + field))
+      }
     }
+  }
 
-    const sortUpdate = () => sortFields('updated')
-    const sortName = () => sortFields('name')
-    const sortCards = () => sortFields('cardsCount')
-    const sortUserName = () => sortFields('user_name')
+  const sortUpdate = () => sortFields('updated')
+  const sortName = () => sortFields('name')
+  const sortCards = () => sortFields('cardsCount')
+  const sortUserName = () => sortFields('user_name')
 
-    return (
-        <div className={styles.table}>
-            <div className={`${styles.header} ${styles.item}`}>
-                <div onClick={sortName} >Name</div>
-                <div onClick={sortCards}>Cards</div>
-                <div onClick={sortUpdate} className={styles.active}>Last Updated</div>
-                <div onClick={sortUserName}>Created by</div>
-                <div>Actions</div>
-            </div>
-            {packs.length > 0
-                ? packs.map(pack => <Pack key={pack._id} pack={pack}/>)
-                : <div style={{padding: '16px 24px'}}>Ничего не найдено</div>}
-        </div>
+  return (
+    <div className={styles.table}>
+      <div className={`${styles.header} ${styles.item}`}>
+        <div onClick={sortName} className={activeField === "name" ? `${styles.active} ${rotate}` : ""} >Name</div>
+        <div onClick={sortCards} className={activeField === "cardsCount" ? `${styles.active} ${rotate}` : ""}>Cards</div>
+        <div onClick={sortUpdate} className={activeField === "updated" ? `${styles.active} ${rotate}` : ""}>Last Updated</div>
+        <div onClick={sortUserName} className={activeField === "user_name" ? `${styles.active} ${rotate}` : ""}>Created by</div>
+        <div>Actions</div>
+      </div>
+      {packs.length > 0
+        ? packs.map(pack => <Pack key={pack._id} pack={pack}/>)
+        : <div style={{padding: '16px 24px'}}>Ничего не найдено</div>}
+    </div>
 
 
-    );
+  );
 };
 
 export default PacksTable;
