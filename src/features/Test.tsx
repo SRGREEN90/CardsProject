f2c882464e24b394b4fbab70c17d725701e8790c
katrina <katrina.nov13@gmail.com>
2022-03-19 15:26:45 +0300

    added Modal

--- src/features/Test.tsx
+++ src/features/Test.tsx
@@ -7,19 +7,23 @@ import SuperEditableSpan from "../main/ui/common/SuperEditableSpan/SuperEditable
 import {RangeDemo} from "../main/ui/common/SuperRange/RangeDemo";
 import {DoubleCheckbox} from "../main/ui/common/GridinCheckbox/DoubleCheckbox";
 import {Pagination} from "../main/ui/common/Pagination/Pagination";
+import Modal from "../main/ui/common/Modal/Modal";
 
 export const Test = () => {
     const [value, setValue] = useState<string>('')
     const [page, setPage] = useState(1)
+    const [isShownModal, setIsShownModal] = useState<boolean>(true)
     const onChangedPage = (page: number) => {
         setPage(page)
     }
+    const closeModal = () => setIsShownModal(false)
+    const showModal = () => setIsShownModal(true)
     return (
         <div className={s.container}>
             <div>
                 <Pagination totalCount={2211} pageSize={10} currentPage={page} onChangedPage={onChangedPage}/>
             </div>
-            <SuperButton>BUTTON</SuperButton>
+            <SuperButton onClick={showModal}>BUTTON</SuperButton>
             <SuperCheckbox/>
             <SuperInputText/>
             <div>
@@ -31,7 +35,12 @@ export const Test = () => {
 
             </div>
             <RangeDemo/>
-
+            <Modal title={'Delete Pack'} show={isShownModal} closeModal={closeModal}>
+                <p>Do you really want to remove Pack Name - Name Pack?
+                    All cards will be excluded from this course.</p>
+                <SuperButton onClick={closeModal}>Cancel</SuperButton>
+                <SuperButton>Delete</SuperButton>
+            </Modal>
         </div>
     );
 };
